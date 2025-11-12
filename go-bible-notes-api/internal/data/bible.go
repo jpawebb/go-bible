117d3cd5bed65b201cb43ab56254bce9d2349adc
James Webb <jameswebb@jamess-mbp.home>
2025-11-12 11:55:55 +0000

    Initialise go-bible

---
+++ go-bible-notes-api/internal/data/bible.go
@@ -0,0 +1,25 @@
+type Book struct {
+    ID   			int
+    Name 			string
+	Chapters		int
+}
+
+
+type Verse struct {
+    ID            	int
+    BookID        	int
+    ChapterNumber 	int
+    VerseNumber   	int
+    KJVText       	string
+    ESVText       	string
+}
+
+
+type Note struct {
+    ID      		int `json:"id"`
+    VerseID 		int `json:"verse_id"`
+    UserID  		int `json:"user_id"`
+    Content 		string `json:"content"`
+    CreatedAt 		time.Time `json:"created_at"`
+    UpdatedAt 		time.Time `json:"updated_at"`
+}
---
+++ go-bible-notes-api/internal/data/loader.go
@@ -0,0 +1,1 @@
+// Chunk bible data into the Postrgres database
